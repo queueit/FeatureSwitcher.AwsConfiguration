3c65ec90ac293983f7a3d2ba9592ccf42dd55dd8
Martin Larsen <ml@prearrangedchaos.com>
2019-10-09 12:31:36 +0200

    Added Integer behaviour

---
+++ src/FeatureSwitcher.AwsConfiguration/Behaviours/IntegerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+
+namespace FeatureSwitcher.AwsConfiguration.Behaviours
+{
+    public abstract class IntegerBehaviour : IBehaviour
+    {
+        protected int ConfiguredValue = 0;
+
+        public IntegerBehaviour()
+        { }
+
+        public IntegerBehaviour(int value)
+        {
+            ConfiguredValue = value;
+        }
+
+        public void SetConfiguration(dynamic configValue)
+        {
+            if (configValue == null)
+                return;
+
+            try
+            {
+                ConfiguredValue = configValue["N"];
+            }
+            catch (RuntimeBinderException)
+            {
+                // fallback to 0;
+            }
+        }
+
+        public bool? Behaviour(Feature.Name name)
+        {
+            return IsEnabled();
+        }
+
+        protected abstract bool IsEnabled();
+    }
+}
---
+++ test/FeatureSwitcher.AwsConfiguration.Tests/Behaviours/IntegerBehaviourTest.cs
@@ -0,0 +1,64 @@
+﻿using FeatureSwitcher.AwsConfiguration.Behaviours;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace FeatureSwitcher.AwsConfiguration.Tests.Behaviours
+{
+    public class IntegerBehaviourTest
+    {
+        [Fact]
+        public void IntegerBehaviour_Deserialization_SameValue_Test()
+        {
+            var data = JObject.Parse("{\"N\": \"45\"}");
+
+            var inList = new TestIntegerBehaviour(45);
+            inList.SetConfiguration(data);
+
+            var enabled = inList.Behaviour(new Feature.Name(typeof (TestFeature1), typeof (TestFeature1).FullName));
+
+            Assert.True(enabled);
+        }
+
+        [Fact]
+        public void IntegerBehaviour_Deserialization_OtherValue_Test()
+        {
+            var data = JObject.Parse("{\"N\": \"37\"}");
+
+            var inList = new TestIntegerBehaviour(45);
+            inList.SetConfiguration(data);
+
+            var enabled = inList.Behaviour(new Feature.Name(typeof(TestFeature1), typeof(TestFeature1).FullName));
+
+            Assert.False(enabled);
+        }
+
+        [Fact]
+        public void IntegerBehaviour_Deserialization_InvalidValue_Test()
+        {
+            var data = JObject.Parse("{\"S\": \"Invalid\"}");
+
+            var inList = new TestIntegerBehaviour(45);
+            inList.SetConfiguration(data);
+
+            var enabled = inList.Behaviour(new Feature.Name(typeof(TestFeature1), typeof(TestFeature1).FullName));
+
+            Assert.False(enabled);
+        }
+
+    }
+
+    public class TestIntegerBehaviour : IntegerBehaviour
+    {
+        private readonly int _value;
+
+        public TestIntegerBehaviour(int value)
+        {
+            _value = value;
+        }
+
+        protected override bool IsEnabled()
+        {
+            return ConfiguredValue == this._value;
+        }
+    }
+}
